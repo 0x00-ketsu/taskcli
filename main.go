4c70e9ca9f1537aab6aec075901861d6bd264ab8
0x00-ketsu <ketsu.chin.121@gmail.com>
2022-10-30 16:19:39 +0800

    feat: check runtime OS

--- main.go
+++ main.go
@@ -1,8 +1,19 @@
 package main
 
-import "github.com/0x00-ketsu/taskcli/internal/cmd"
+import (
+	"fmt"
+	"os"
+	"runtime"
+
+	"github.com/0x00-ketsu/taskcli/internal/cmd"
+)
 
 
 func main() {
+	if runtime.GOOS == "windows" {
+		fmt.Println("Can't execute this on a windows machine")
+		os.Exit(0)
+	}
+
 	cmd.Execute()
 }
